dc8d0572dc3ca80499032f7c15529b20e95e2ddf
JohnRyanFleming <71190808+JohnRyanFleming@users.noreply.github.com>
2021-10-07 14:52:41 -0400

    Created Table for Recipes and a recipe database file
    Basic Framework for Recipes has been created. Sqlite properly creates the recipebase.db file, should be shared across all users for testing purposes. Ingredients Table is commented out for now, design framework for ingredients will be developed in Implementation #2

--- Project basis build - not complete/setup.py
+++ Project basis build - not complete/setup.py
@@ -7,13 +7,13 @@ import sqlite3
 conn = sqlite3.connect('recipebase.db')
 print("Opened database successfully")
 
-conn.execute('CREATE TABLE Recipe (RecipeID INTEGER, Name TEXT, Ingredients)')
+conn.execute('CREATE TABLE Recipe (RecipeID INTEGER, Name TEXT, Steps Text, Ingredients TEXT)')
 print ("Reviews table created successfully")
 
 
-conn.execute('CREATE TABLE Ingredients ()')
+# conn.execute('CREATE TABLE Ingredients ()') # Commented for now, not used in implementation 1
 
 
-print ("Ratings Table created Successfully")
+# print ("Ratings Table created Successfully") # Commented for now, not used in implementation 1
 
 conn.close()
