c4da1a2d9048ff80e438d81ebd151d2d11449480
jrn18 <77980892+jrn18@users.noreply.github.com>
2021-11-04 13:40:41 -0400

    Pantry Functionality
    Added Functionality for adding and accessing the Pantry. Currently the Measurement is unitless. UserID is also set statically to 1.

--- Project basis build/app.py
+++ Project basis build/app.py
@@ -10,21 +10,21 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/enternew')
+@app.route('/addRecipe')
 def write_recipe():
 	return render_template('addRecipe.html')
+
+@app.route('/addToPantry')
+def write_Pantry():
+	return render_template('addPantry.html')
 	
 @app.route("/getRecipe")
 def getrecipe():
 	return render_template('getRecipe.html')
 
-@app.route('/addpant')
-def addpant():
-	return render_template('addPantry.html')
 
-@app.route('/listpant')
-def listpant():
-	return render_template('listpant.html')
+
+
 	
 @app.route('/getrec',methods = ['POST','GET'])
 def getrec():
@@ -54,7 +54,16 @@ def getrec():
 	
 	
 
-	
+@app.route('/listpant')
+def listpant():
+	con = sql.connect("recipebase.db")
+	con.row_factory = sql.Row		
+	cur = con.cursor()
+
+	cur.execute("SELECT Pantry.Name, Pantry.Quantity, Pantry.Measurement FROM Pantry ORDER BY Pantry.Name DESC ")
+
+	rows = cur.fetchall()
+	return render_template('listpant.html', rows = rows)
 
 
 @app.route('/list')
@@ -63,7 +72,7 @@ def list():
 	con.row_factory = sql.Row
 	cur = con.cursor()
 	
-	cur.execute("SELECT * FROM recipe ORDER BY Name DESC ")
+	cur.execute("SELECT * FROM Recipe ORDER BY Name DESC ")
 	
 	rows = cur.fetchall()
 	return render_template("list.html",rows = rows)
@@ -94,6 +103,33 @@ def addrec():
 		finally:
 			return render_template("result.html",msg = msg)
 			con.close()
+
+@app.route('/addpant',methods = ['POST','GET'])
+def addpant():
+	print("attempting to add ingredient")
+	if request.method == 'POST':
+		try:
+			Name = request.form['Name']
+			Quantity = request.form['Quantity']
+			Measurement = request.form['Measurement']
+			print ("UserID: ",1,"Name: ",Name,"Quantity: ",Quantity,"Measurement: ",Measurement,"\n")
+
+			with sql.connect("recipebase.db") as con:
+				cur = con.cursor()
+				cur.execute("INSERT INTO Pantry (UserID,Name,Quantity,Measurement) VALUES (?,?,?,?)", (1,Name, Quantity, Measurement))
+				con.commit()
+				
+				msg = "Ingredient Successfully added to Pantry"
+
+		except:
+			con.rollback()
+			msg = "error in insert operations"
+
+		finally:
+			return render_template('result.html',msg = msg)
+			con.close()
+	
+
 
 
 if __name__ == '__main__':
